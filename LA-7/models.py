7437ff097ba72e4daee01e5125cdc0452a15dc58
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-20 20:44:13 +0530

    updated models names

--- LA-7/models.py
+++ LA-7/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
-class Student(db.Model):
+class student(db.Model):
     student_id = db.Column(db.Integer, primary_key = True, nullable = False)
     roll_number = db.Column(db.String, unique = True, nullable = False)
     first_name = db.Column(db.String, nullable = False)
@@ -12,16 +12,16 @@ class Student(db.Model):
     def __repr__(self):
         return "<Student %r>" % self.roll_number
 
-class Course(db.Model):
+class course(db.Model):
     course_id = db.Column(db.Integer, primary_key = True, nullable = False)
     course_code = db.Column(db.String, nullable = False, unique = True)
     course_name = db.Column(db.String, nullable = False)
-    description = db.Column(db.String)
+    course_description = db.Column(db.String)
 
     def __repr__(self):
         return "<Course %r>" % self.course_code
 
-class Enrollments(db.Model):
+class enrollments(db.Model):
     enrollment_id = db.Column(db.Integer, primary_key = True, nullable = False)
     estudent_id = db.Column(db.Integer, ForeignKey('student.student_id'), nullable = False)
     ecourse_id = db.Column(db.Integer, ForeignKey('course.course_id'), nullable = False)
