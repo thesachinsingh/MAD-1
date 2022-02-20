d7872f1321f0d54ec61b889e0daffd492d0db157
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-20 16:57:47 +0530

    updated app.py

--- LA-7/app.py
+++ LA-7/app.py
@@ -1,8 +1,17 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
 
+curr_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(curr_dir, 'week7_database.sqlite3')
+db.init_app(app)
+
+@app.route('/')
+def homepage():
+    pass
 
 
 
