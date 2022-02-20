4ca81656abd693354771fcf543ce87cf62f9f492
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-21 00:09:52 +0530

    added relationships and  updated foreignkey

--- LA-7/models.py
+++ LA-7/models.py
@@ -8,6 +8,7 @@ class student(db.Model):
     roll_number = db.Column(db.String, unique = True, nullable = False)
     first_name = db.Column(db.String, nullable = False)
     last_name = db.Column(db.String)
+    s_enrollment = db.relationship("enrollments", backref="stud")
 
     def __repr__(self):
         return "<Student %r>" % self.roll_number
@@ -17,14 +18,16 @@ class course(db.Model):
     course_code = db.Column(db.String, nullable = False, unique = True)
     course_name = db.Column(db.String, nullable = False)
     course_description = db.Column(db.String)
+    c_enrollment = db.relationship("enrollments", backref="cours")
 
     def __repr__(self):
         return "<Course %r>" % self.course_code
 
 class enrollments(db.Model):
     enrollment_id = db.Column(db.Integer, primary_key = True, nullable = False)
-    estudent_id = db.Column(db.Integer, ForeignKey('student.student_id'), nullable = False)
-    ecourse_id = db.Column(db.Integer, ForeignKey('course.course_id'), nullable = False)
-
+    estudent_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable = False)
+    ecourse_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), nullable = False)
+    #stud
+    #cours
     def __repr__(self):
         return "<Enrollment %r>" % self.enrollment_id
